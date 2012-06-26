1b861eed93c0d9576ecf8f7d46d91859a8aab963
Vidhuran Harichandra Babu <vidhuran.harichandra.babu@ericsson.com>
2012-06-26 17:38:07 +0300

    Intial commit for GamerTest class

--- src/main/java/dojo/dependency/Gamer.java
+++ src/main/java/dojo/dependency/Gamer.java
@@ -3,27 +3,38 @@ package dojo.dependency;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.PrintStream;
 
 public class Gamer {
 
     public static void main(String... args) {
-            new Gamer().play();
+            new Gamer(new BufferedReader(new InputStreamReader(System.in)),new FizzBuzz(),new PrintStream(System.out)).play();
     }
 
+	private BufferedReader br;
+	private FizzBuzz fizzBuzz;
+	private PrintStream out;
+
+    public Gamer(BufferedReader br, FizzBuzz fizzBuzz, PrintStream out){
+    	this.br = br;
+    	this.fizzBuzz = fizzBuzz;
+    	this.out = out;
+    }
+    
     public void play() {
             String input = read();
-            FizzBuzz fizzBuzz = new FizzBuzz();
+            // FizzBuzz fizzBuzz = new FizzBuzz();
             int pos = input.indexOf(',');
             int start = Integer.parseInt(input.substring(0, pos));
             int stop = Integer.parseInt(input.substring(pos + 1));
             for (String element : fizzBuzz.play(start, stop)) {
-                    System.out.printf("%s ", element);
+                    out.printf("%s", element);
             }
     }
 
     public String read() {
-            System.out.println("fizz buzz: ");
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            out.println("fizz buzz: ");
+            // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
             try {
                     return br.readLine();
             } catch (IOException ioe) {
